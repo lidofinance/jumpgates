25cde406f8fd37e65f9e21569983d461805d17f5
Azat Serikov <azatsdev@gmail.com>
2022-03-02 14:18:55 +0600

    refactor: refactor mock token deploy

--- scripts/deploy_mock_erc20_token.py
+++ scripts/deploy_mock_erc20_token.py
@@ -1,32 +1,5 @@
-from eth_utils import from_wei, to_wei
-import utils.log as log
-from utils.account import get_account
-from brownie import network, MockERC20Token
-from utils.network import is_dev
+from utils.contract import deploy_mock_erc20_token
 
 
 def main():
-    log.info("Checking network...")
-    if network.show_active() == "mainnet":
-        log.error("Shouldn't deploy a mock ERC20 token on mainnet. Aborting...")
-
-    log.okay(f"Network is {network.show_active()}. Proceeding...")
-
-    deployer = get_account()
-
-    log.info("Deployer balance", from_wei(deployer.balance(), "ether"))
-
-    dev = is_dev()
-    one_billion = to_wei(10**9, "ether")
-    log.info("Deploying MockERC20Token...")
-
-    mock_erc20_token = MockERC20Token.deploy(
-        one_billion, {"from": deployer}, publish_source=not dev
-    )
-
-    log.okay("Deploy complete!")
-
-    log.info(
-        "Deployer MockERC20Token balance",
-        from_wei(mock_erc20_token.balanceOf(deployer.address), "ether"),
-    )
+    deploy_mock_erc20_token()
--- utils/contract.py
+++ utils/contract.py
@@ -1,6 +1,10 @@
-from brownie import Contract
-from utils.config import wormhole_token_bridge, ldo
+from brownie import MockERC20Token, Contract, network
+from utils.config import WORMHOLE_TOKEN_BRIDGE_ADDRESS, ERC20_TOKEN_ADDRESS
 import json
+from eth_utils import from_wei, to_wei
+import utils.log as log
+from utils.account import get_account
+from utils.network import is_dev
 
 
 def get_wormhole_token_bridge_contract(chain_id):
@@ -8,9 +12,36 @@ def get_wormhole_token_bridge_contract(chain_id):
         abi = json.loads(abi_json.read())
 
     return Contract.from_abi(
-        "Wormhole: Token Bridge", wormhole_token_bridge.get(chain_id), abi
+        "Wormhole: Token Bridge", WORMHOLE_TOKEN_BRIDGE_ADDRESS.get(chain_id), abi
     )
 
 
-def get_ldo_contract(chain_id, owner):
-    return Contract.from_explorer(ldo.get(chain_id), owner=owner)
+def get_erc20_token_contract(chain_id, owner):
+    return Contract.from_explorer(ERC20_TOKEN_ADDRESS.get(chain_id), owner=owner)
+
+
+def deploy_mock_erc20_token():
+    log.info("Checking network...")
+    if network.show_active() == "mainnet":
+        log.error("Shouldn't deploy a mock ERC20 token on mainnet. Aborting...")
+
+    log.okay(f"Network is {network.show_active()}. Proceeding...")
+
+    deployer = get_account()
+
+    log.info("Deployer balance", from_wei(deployer.balance(), "ether"))
+
+    dev = is_dev()
+    one_billion = to_wei(10**9, "ether")
+    log.info("Deploying MockERC20Token...")
+
+    mock_erc20_token = MockERC20Token.deploy(
+        one_billion, {"from": deployer}, publish_source=not dev
+    )
+
+    log.okay("Deploy complete!")
+
+    log.info(
+        "Deployer MockERC20Token balance",
+        from_wei(mock_erc20_token.balanceOf(deployer.address), "ether"),
+    )
