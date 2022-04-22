c016b10dac6edf5a87aefa2c7fd85ffe9b2779d9
Azat Serikov <azatsdev@gmail.com>
2022-04-22 11:03:14 +0600

    test: create mock erc20 with nonstandard decimals

---
+++ contracts/test_helpers/MockERC20WithNonStandardDecimals.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "OpenZeppelin/openzeppelin-contracts@4.5.0/contracts/token/ERC20/ERC20.sol";
+
+/// @title Mock ERC20
+/// @author mymphe
+/// @notice a mock ERC20 token for unit tests
+contract MockERC20WithNonStandardDecimals is ERC20 {
+    uint8 private immutable decimalNumber;
+
+    constructor(uint8 _decimals) ERC20("Mock ERC20", "M20") {
+        require(_decimals <= type(uint8).max);
+
+        _mint(msg.sender, 1000000000 ether);
+        decimalNumber = _decimals;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimalNumber;
+    }
+}
